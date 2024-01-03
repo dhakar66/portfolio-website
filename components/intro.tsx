5bb303d4e2be0a51c9b4b874353492aebbc8e6e2
tenzin dhakar <85844725+dhakar66@users.noreply.github.com>
2024-01-04 01:07:24 +0530

    added my CV and some more skills

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
         {/* for the download CV button  */}
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href=""
+          href="/Tenzin_Dhakar_Resume.pdf"
           download
         >
           Download CV{" "}
